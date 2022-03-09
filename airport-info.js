c40aa859e1958683bb45cb606511f3bbab3f720a
Ben P <70548361+cloudcautionx@users.noreply.github.com>
2022-03-09 22:04:43 +0000

    good

--- airport-info.js
+++ airport-info.js
@@ -1,5 +1,29 @@
-const getAirportInfo = async() => {
-    const fetch = require("node-fetch")
+const fetch = require("node-fetch")
 
-    
+const getAirportInfo = async(query) => {
+    const response = {
+        name: "",
+        country: "",
+        city: "",
+        timezone: "",
+        icao: "",
+        latitude: 0,
+        longitude: 0
+    }
+
+    const airportApi = await fetch(`https://api.aviowiki.com/free/airports/search?query=${query}`)
+        .then(res => res.json())
+        .catch(e => console.error(e))
+
+    response.name = airportApi.content[0].name
+    response.country = airportApi.content[0].country.name
+    response.city = airportApi.content[0].servedCity
+    response.timezone = airportApi.content[0].timeZone
+    response.icao = airportApi.content[0].icao
+    response.latitude = airportApi.content[0].coordinates.latitude
+    response.longitude = airportApi.content[0].coordinates.longitude
+
+    return response
 }
+
+module.exports = getAirportInfo;
